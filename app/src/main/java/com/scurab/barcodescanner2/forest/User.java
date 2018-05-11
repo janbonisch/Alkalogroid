a980fea88fe31c26b49cffbfdafee76adc3ad57d
janbonisch <jan.bonisch@gmail.com>
2018-05-11 15:29:08 +0200

    zmena popisu uzivatele

--- app/src/main/java/com/scurab/barcodescanner2/forest/User.java
+++ app/src/main/java/com/scurab/barcodescanner2/forest/User.java
@@ -5,12 +5,9 @@ import com.google.gson.annotations.SerializedName;
 // Uživatel
 public class User {
     // key, required, length = 20
-    @SerializedName("UserName")
     public String UserName;
     // required, length = 20
-    @SerializedName("DisplayName")
     public String DisplayName;
     // required, default 0
-    @SerializedName("Credit")
-    public double Credit;
+    public double DtConsStart;
 }
